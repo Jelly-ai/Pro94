e3b1493475cc2d41c2f3e48c6e6d9492066a87e0
Jelly-ai <73816313+Jelly-ai@users.noreply.github.com>
2021-01-12 13:05:51 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDo7z-VQXOu2mM4wWeuYppjVZumOGUm1Pc",
+    authDomain: "kwitter-project-85d17.firebaseapp.com",
+    databaseURL: "https://kwitter-project-85d17-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-85d17",
+    storageBucket: "kwitter-project-85d17.appspot.com",
+    messagingSenderId: "660105855646",
+    appId: "1:660105855646:web:0e00e9357776243840b18f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
